3f86a11119e19612e141655107bbd0e95160c662
dmartin <dmartin@accesgroup.com.mx>
2025-07-24 09:58:21 -0600

    [style] cambios en la descripcion de las variables

--- variables.tf
+++ variables.tf
@@ -1,47 +1,47 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "Region de AWS donde se desplegara la infraestructura"
   type        = string
   default     = "us-east-1"
 }
 
 variable "vpc_cidr" {
-  description = "VPC CIDR block"
+  description = "CIDR de la VPC"
   type        = string
   default     = "10.0.0.0/16"
 }
 
 variable "public_subnets" {
-  description = "Public subnets CIDR"
+  description = "CIDR de las subredes publicas"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
-  description = "Private subnets CIDR"
+  description = "CIDR de las subredes privadas"
   type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "app_instances" {
-  description = "App instance names"
+  description = "Nombre de las instancias"
   type        = list(string)
   default     = ["App1", "App2", "App3"]
 }
 
 variable "instance_type" {
-  description = "EC2 instance type"
+  description = "Tipo de instancia EC2"
   type        = string
   default     = "t3.micro"
 }
 
 variable "key_name" {
-  description = "SSH key pair name"
+  description = "Nombre del KeyPair para acceso SSH"
   type        = string
   sensitive   = true
 }
 
 variable "my_ip" {
-  description = "Your public IP for SSH"
-  type        = string
-  sensitive   = true
+  description = "Tu IP publica para conectarte a SSH"
+  type = string
+  sensitive = true 
 }
